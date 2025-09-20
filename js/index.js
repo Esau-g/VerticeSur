112ddfa0c88eae5a9408782ffe090a1d3d4084a1
Esau <esaugarciaph@gmail.com>
2025-09-20 16:47:36 -0300

    faq changes

--- js/index.js
+++ js/index.js
@@ -24,3 +24,14 @@ video.play().catch(() => {
     window.addEventListener("scroll", startVideo, { once: true });
     window.addEventListener("click", startVideo, { once: true });
 });
+
+const q = document.querySelectorAll('.q');
+const a = document.querySelectorAll('.a');
+const arr = document.querySelectorAll('.arrow');
+
+for(let i = 0; i < q.length; i++) {
+    q[i].addEventListener('click', () => {
+        a[i].classList.toggle('a-opened');
+        arr[i].classList.toggle('arrow-rotated');
+    });
+}
